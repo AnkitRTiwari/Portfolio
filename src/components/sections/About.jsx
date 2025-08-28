efbebda4fe5bcf8d61894ef3d47126dd7ea9b05e
AnkitAspp <ankitrtiwari2@gmail.com>
2025-08-28 16:33:21 +0530

    chore: initial commit

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,96 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "React",
+    "Javascript",
+    "TypeScript",
+    "TailwindCSS",
+    "Redux",
+  ];
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Streamlined application development by developing reusable and
+              generic components using React, resulting in a 30% reduction in
+              development time • Created a scalable UI theme with Tailwind CSS
+              in React, improving design consistency and development efficiency.
+            </p>
+
+            <a
+              href="/public/Ankit Tiwari's Resume.pdf"
+              target="_blank"
+              className="bg-blue-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] hover:-translate-y-1 transition-all hover:text-white"
+            >
+              View Resume
+            </a>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] hover:-translate-y-1 transition-all"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong>
+                    JavaScript Algorithms and Data Structures (Beta){" "}
+                  </strong>{" "}
+                  certification from Free Code camp.
+                </li>
+                <li>
+                  <strong> B com </strong> - Sydenham College of Commerce and
+                  Economics (2015 - 2018)
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Software Engineer at Zeal Interactive Service (2022 -
+                    Present){" "}
+                  </h4>
+                  <p>
+                    Collaborated with product managers, designers, and backend
+                    teams to translate UI/UX requirements into scalable React
+                    components, ensuring smooth integration with REST APIs and
+                    consistent user experience across devices
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
