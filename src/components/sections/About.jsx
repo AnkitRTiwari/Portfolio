f365b17533c20ce0c55aaa86a7130e03584fc188
AnkitAspp <ankitrtiwari2@gmail.com>
2025-08-28 17:08:34 +0530

    change the pdf url

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -30,7 +30,7 @@ export const About = () => {
             </p>
 
             <a
-              href="/public/resume.pdf"
+              href="/resume.pdf"
               target="_blank"
               className="bg-blue-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] hover:-translate-y-1 transition-all hover:text-white"
             >
